15ae38e52f19fb651bab9839ef320a741caa0c07
Ciaran R <ciaranroe1@gmail.com>
2024-09-19 22:36:57 +0100

    exercise 3
    reverse string function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,21 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let arr = [];
+    if(str === ""){
+        return "";
+    }
+    for(let i = 0; i < str.length; i++){
+        let arr = [];
+        let revStr = '';
+        for(let i = str.length; i >= 0; i--){
+            arr.push(str.charAt(i));
+        }
 
+        for(let i = 0; i < arr.length; i++){
+            revStr += arr[i];
+        }
+
+        return revStr;
+    }
 };
 
 // Do not edit below this line
